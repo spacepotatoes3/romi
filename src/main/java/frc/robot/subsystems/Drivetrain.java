f136736b9e063d290050916c819138ef6a4947db
Briar Chang <briartchang@gmail.com>
2024-08-31 16:45:37 -0700

    INITIAL COMMIT

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -11,10 +11,24 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.romi.RomiGyro;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+//import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.math.geometry.Pose2d;
+//import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Drivetrain extends SubsystemBase {
   private static final double kCountsPerRevolution = 1440.0;
-  private static final double kWheelDiameterCm = 7; // 70 mm
+  private static final double kWheelDiameterInch = 2.75591; // 70 mm
+  private static final double kWheelDiameterMeter = Units.inchesToMeters(kWheelDiameterInch);
+
+  private final DifferentialDriveKinematics m_kinematics = 
+    new DifferentialDriveKinematics(Units.inchesToMeters(8.0));
 
   // The Romi has the left and right motors set to
   // PWM channels 0 and 1 respectively
@@ -36,8 +50,25 @@ public class Drivetrain extends SubsystemBase {
   // Set up the BuiltInAccelerometer
   private final BuiltInAccelerometer m_accelerometer = new BuiltInAccelerometer();
 
+  private final Field2d m_fieldApproximation = new Field2d(); //may need to initialize pose
+
+  // Set up pose estimator
+  /* Here we use DifferentialDrivePoseEstimator so that we can fuse odometry readings. The
+  numbers used  below are robot specific, and should be tuned. */
+  private final DifferentialDrivePoseEstimator m_poseEstimator =
+      new DifferentialDrivePoseEstimator(
+          m_kinematics,
+          new Rotation2d(m_gyro.getAngle()),
+          m_leftEncoder.getDistance(),
+          m_rightEncoder.getDistance(),
+          new Pose2d(),
+          VecBuilder.fill(0.05, 0.05, Units.degreesToRadians(5)),
+          VecBuilder.fill(0.5, 0.5, Units.degreesToRadians(30)));
+
+
   /** Creates a new Drivetrain. */
   public Drivetrain() {
+    
     SendableRegistry.addChild(m_diffDrive, m_leftMotor);
     SendableRegistry.addChild(m_diffDrive, m_rightMotor);
 
@@ -46,10 +77,14 @@ public class Drivetrain extends SubsystemBase {
     // gearbox is constructed, you might have to invert the left side instead.
     m_rightMotor.setInverted(true);
 
-    // Use centimeters as unit for encoder distances
-    m_leftEncoder.setDistancePerPulse((Math.PI * kWheelDiameterCm) / kCountsPerRevolution);
-    m_rightEncoder.setDistancePerPulse((Math.PI * kWheelDiameterCm) / kCountsPerRevolution);
+    // Use inches as unit for encoder distances
+    m_leftEncoder.setDistancePerPulse((Math.PI * kWheelDiameterMeter) / kCountsPerRevolution);
+    m_rightEncoder.setDistancePerPulse((Math.PI * kWheelDiameterMeter) / kCountsPerRevolution);
     resetEncoders();
+    m_gyro.reset();
+
+    SmartDashboard.putData("FieldEstimation", m_fieldApproximation);
+
   }
 
   public void arcadeDrive(double xaxisSpeed, double zaxisRotate) {
@@ -69,16 +104,16 @@ public class Drivetrain extends SubsystemBase {
     return m_rightEncoder.get();
   }
 
-  public double getLeftDistanceCm() {
-    return m_leftEncoder.getDistance() * 2.54;
+  public double getLeftDistanceInch() {
+    return m_leftEncoder.getDistance();
   }
 
-  public double getRightDistanceCm() {
-    return m_rightEncoder.getDistance() * 2.54;
+  public double getRightDistanceInch() {
+    return m_rightEncoder.getDistance();
   }
 
-  public double getAverageDistanceCm() {
-    return (getLeftDistanceCm() + getRightDistanceCm()) / 2.0;
+  public double getAverageDistanceInch() {
+    return (getLeftDistanceInch() + getRightDistanceInch()) / 2.0;
   }
 
   /**
@@ -140,8 +175,39 @@ public class Drivetrain extends SubsystemBase {
     m_gyro.reset();
   }
 
+
+  /** Updates the field-relative position. */
+  public void updateOdometry() {
+    m_poseEstimator.update(
+        new Rotation2d(m_gyro.getAngle()), m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
+
+    // Publish cameraToObject transformation to networktables --this would normally be handled by
+    // the
+    // computer vision solution.
+    //publishCameraToObject(
+    //    m_objectInField, m_robotToCamera, m_cameraToObjectEntry, m_drivetrainSimulator);
+
+    // Compute the robot's field-relative position exclusively from vision measurements.
+    //Pose3d visionMeasurement3d =
+    //    objectToRobotPose(m_objectInField, m_robotToCamera, m_cameraToObjectEntry);
+
+    // Convert robot pose from Pose3d to Pose2d needed to apply vision measurements.
+    //Pose2d visionMeasurement2d = visionMeasurement3d.toPose2d();
+
+    // Apply vision measurements. For simulation purposes only, we don't input a latency delay -- on
+    // a real robot, this must be calculated based either on known latency or timestamps.
+    //m_poseEstimator.addVisionMeasurement(visionMeasurement2d, Timer.getFPGATimestamp());
+     
+    m_fieldApproximation.setRobotPose(m_poseEstimator.getEstimatedPosition());
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    updateOdometry();
+    
   }
+
+
+
 }
